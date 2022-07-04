a58edb06053e10c04e53b29f3019ccd7e9cd5e1a
Kenny Baron <kennybaron@fastmail.com>
2022-07-04 18:44:35 -0400

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,28 +2,27 @@
 
 set -e  # -e: exit on error
 
-# if [ ! "$(command -v chezmoi)" ]; then
-  # bin_dir="$HOME/bin"
-  # chezmoi="$bin_dir/chezmoi"
-  # if [ "$(command -v curl)" ]; then
-    # sh -c "$(curl -fsLS https://chezmoi.io/get)" -- -b "$bin_dir"
-  # elif [ "$(command -v wget)" ]; then
-    # sh -c "$(wget -qO- https://chezmoi.io/get)" -- -b "$bin_dir"
-  # else
-    # echo "To install chezmoi, you must have curl or wget installed." >&2
-    # exit 1
-  # fi
-# else
-  # chezmoi=chezmoi
-# fi
-
-sh -c "$(curl -fsLS https://chezmoi.io/get)" -- -b "$bin_dir"
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsLS https://chezmoi.io/get)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://chezmoi.io/get)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
-# exec "$chezmoi" init --apply "--source=$script_dir"
+exec "$chezmoi" init --apply "--source=$script_dir"
 
-exec /dotfilez/chezmoi init --apply "--source=$script_dir"
+# Alternate for debugging
+# exec /dotfilez/chezmoi init --apply "--source=$script_dir"
 
 chsh -s $(which zsh)
