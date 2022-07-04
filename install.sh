290949f92df250101000c99448be76c16fd1dd77
Kenny Baron <kennybaron@fastmail.com>
2022-07-04 18:12:14 -0400

    fix install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ ! "$(command -v chezmoi)" ]; then
   if [ "$(command -v curl)" ]; then
     sh -c "$(curl -fsLS https://chezmoi.io/get)" -- -b "$bin_dir"
   elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -q0- https://chezmoi.io/get)" -- -b "$bin_dir"
+    sh -c "$(wget -qO- https://chezmoi.io/get)" -- -b "$bin_dir"
   else
     echo "To install chezmoi, you must have curl or wget installed." >&2
     exit 1
