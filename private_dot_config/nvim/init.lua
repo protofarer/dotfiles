dd13d246bc8c6cd1a1913dc962865d1755ac679c
Kenny Baron <kennybaron@fastmail.com>
2024-05-27 10:36:01 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -777,22 +777,22 @@ require("lazy").setup({
 						use_as_default_explorer = true,
 					},
 				},
-				-- keys = {
-				-- 	{
-				-- 		"<leader>fm",
-				-- 		function()
-				-- 			require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-				-- 		end,
-				-- 		desc = "Open mini.files (Directory of Current File)",
-				-- 	},
-				-- 	{
-				-- 		"<leader>fM",
-				-- 		function()
-				-- 			require("mini.files").open(vim.uv.cwd(), true)
-				-- 		end,
-				-- 		desc = "Open mini.files (cwd)",
-				-- 	},
-				-- },
+				keys = {
+					{
+						"<leader>fm",
+						function()
+							require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+						end,
+						desc = "Open mini.files (Directory of Current File)",
+					},
+					-- {
+					-- 	"<leader>fM",
+					-- 	function()
+					-- 		require("mini.files").open(vim.uv.cwd(), true)
+					-- 	end,
+					-- 	desc = "Open mini.files (cwd)",
+					-- },
+				},
 				config = function(_, opts)
 					require("mini.files").setup({ opts })
 
