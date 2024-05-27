40d7741fc6f305d26879f38b73f391beaa26875a
Kenny Baron <kennybaron@fastmail.com>
2024-05-26 23:22:36 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -194,18 +194,18 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	end,
 })
 
--- vim.api.nvim_create_autocmd('ColorScheme', {
---   pattern = 'kanagawa',
---   callback = function()
---     if vim.o.background == 'light' then
---       vim.fn.system 'kitty +kitten themes Kanagawa_light'
---     elseif vim.o.background == 'dark' then
---       vim.fn.system 'kitty +kitten themes Kanagawa_dragon'
---     else
---       vim.fn.system 'kitty +kitten themes Kanagawa'
---     end
---   end,
--- })
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "kanagawa",
+	callback = function()
+		if vim.o.background == "light" then
+			vim.fn.system("kitty +kitten themes Kanagawa_light")
+		elseif vim.o.background == "dark" then
+			vim.fn.system("kitty +kitten themes Kanagawa_dragon")
+		else
+			vim.fn.system("kitty +kitten themes Kanagawa")
+		end
+	end,
+})
 
 -- Use `opts = {}` to force a plugin to be loaded.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
