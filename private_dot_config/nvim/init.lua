77f0b01af9778343987f8ca43b83b61345df9f43
Kenny Baron <kennybaron@fastmail.com>
2024-05-27 11:05:15 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -275,8 +275,6 @@ require("lazy").setup({
 				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
 				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
 				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-				-- ["<leader>fm"] = { name = "[f]iles [m]enu mini.files dir of current file", _ = "which_key_ignore" },
-				-- ["<leader>fM"] = { name = "[f]iles [M]enu mini.files with cwd", _ = "which_key_ignore" },
 			})
 			-- visual mode
 			require("which-key").register({
@@ -797,52 +795,34 @@ require("lazy").setup({
 		config = function(_, opts)
 			require("mini.files").setup({ opts })
 
-			-- 	local minifiles_toggle = function()
-			-- 		if not MiniFiles.close() then
-			-- 			MiniFiles.open()
-			-- 		end
-			-- 	end
-			-- 	-- vim.keymap.set("n", "<leader>fm", minifiles_toggle, { desc = "toggle mini.files navigation" })
-			-- 	vim.keymap.set(
-			-- 		"n",
-			-- 		"<leader>fm",
-			-- 		":lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>",
-			-- 		{ desc = "toggle mini.files navigation" }
-			-- 	)
-			--
-			-- 	-- mapping to show/hide dot-files
-			-- 	local show_dotfiles = true
-			-- 	local filter_show = function(fs_entry)
-			-- 		return true
-			-- 	end
-			-- 	local filter_hide = function(fs_entry)
-			-- 		return not vim.startswith(fs_entry.name, ".")
-			-- 	end
-			--
-			-- 	local toggle_dotfiles = function()
-			-- 		show_dotfiles = not show_dotfiles
-			-- 		local new_filter = show_dotfiles and filter_show or filter_hide
-			-- 		require("mini.files").refresh({ content = { filter = new_filter } })
-			-- 	end
-			--
-			-- 	vim.api.nvim_create_autocmd("User", {
-			-- 		pattern = "MiniFilesBufferCreate",
-			-- 		callback = function(args)
-			-- 			local buf_id = args.data.buf_id
-			-- 			vim.keymap.set(
-			-- 				"n",
-			-- 				"g.",
-			-- 				toggle_dotfiles,
-			-- 				{ buffer = buf_id, desc = "Toggle Hidden Files" }
-			-- 			)
-			-- 		end,
-			-- 	})
-			-- 	vim.api.nvim_create_autocmd("User", {
-			-- 		pattern = "MiniFilesActionRename",
-			-- 		callback = function(event)
-			-- 			LazyVim.lsp.on_rename(event.data.from, event.data.to)
-			-- 		end,
-			-- 	})
+			-- mapping to show/hide dot-files
+			local show_dotfiles = true
+			local filter_show = function(_)
+				return true
+			end
+			local filter_hide = function(fs_entry)
+				return not vim.startswith(fs_entry.name, ".")
+			end
+
+			local toggle_dotfiles = function()
+				show_dotfiles = not show_dotfiles
+				local new_filter = show_dotfiles and filter_show or filter_hide
+				require("mini.files").refresh({ content = { filter = new_filter } })
+			end
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "MiniFilesBufferCreate",
+				callback = function(args)
+					local buf_id = args.data.buf_id
+					vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id, desc = "Toggle Hidden Files" })
+				end,
+			})
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "MiniFilesActionRename",
+				callback = function(event)
+					LazyVim.lsp.on_rename(event.data.from, event.data.to)
+				end,
+			})
 		end,
 	},
 	{ -- Highlight, edit, and navigate code ,treesitter
