ed4eac0d2cb0ad4cb80c734fec54984befacf47b
Kenny Baron <kennybaron@fastmail.com>
2024-05-27 10:00:44 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -563,7 +563,7 @@ require("lazy").setup({
 		lazy = false,
 		keys = {
 			{
-				"<leader>f",
+				"<leader>ff",
 				function()
 					require("conform").format({ async = true, lsp_fallback = true })
 				end,
