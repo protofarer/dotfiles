f7f400f507df14ab993401b6d300ad727e623669
Kenny Baron <kennybaron@fastmail.com>
2024-05-29 22:37:55 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -664,22 +664,15 @@ require("lazy").setup({
 				-- TODO:use these cmp nav bindings or tab/s-tab/enter?
 				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
-					-- Select the [n]ext item
 					["<C-n>"] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
 					["<C-p>"] = cmp.mapping.select_prev_item(),
-
 					-- Scroll the documentation window [b]ack / [f]orward
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-
 					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
 					["<Tab>"] = cmp.mapping.confirm({ select = true }),
 
 					-- If you prefer more traditional completion keymaps,
-					-- you can uncomment the following lines
 					--['<CR>'] = cmp.mapping.confirm { select = true },
 					--['<Tab>'] = cmp.mapping.select_next_item(),
 					--['<S-Tab>'] = cmp.mapping.select_prev_item(),
@@ -731,7 +724,6 @@ require("lazy").setup({
 		opts = {
 			theme = "wave",
 			background = { dark = "dragon", light = "wave" },
-			-- remove gutter background
 			colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
 		},
 	},
@@ -939,6 +931,16 @@ require("lazy").setup({
 			shade_terminals = true,
 		},
 	},
+	{ -- improve native ts comments
+		"folke/ts-comments",
+		opts = {},
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
+	},
+	{ -- treesitter autoclose/autorename html tags
+		"windwp/nvim-ts-autotag",
+	},
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 	-- {
 	-- 	"nvimdev/dashboard-nvim",
 	-- 	event = "VimEnter",
