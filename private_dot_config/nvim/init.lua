5b05041eb1af2c02d2f7fd5942275033b9df2b95
Kenny Baron <kennybaron@fastmail.com>
2024-05-31 09:38:25 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1048,7 +1048,7 @@ require("lazy").setup({
 	-- 	},
 	-- },
 	-- { import = "custom.plugins" },
-	{ "custom.gitsigns_contrib" },
+	{ import = "custom.gitsigns_contrib" },
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
