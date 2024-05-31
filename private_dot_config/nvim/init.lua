c4fa88e6315865cf7cb63942cd53dfc8e4ab89ef
Kenny Baron <kennybaron@fastmail.com>
2024-05-31 09:26:22 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -920,12 +920,12 @@ require("lazy").setup({
 	require("kickstart.plugins.gitsigns"), -- adds gitsigns recommend keymaps
 
 	-- Custom ,plugins `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-	-- { -- no out-of-the-box gitsigns integration, pretty useless?
-	-- 	"dstein64/nvim-scrollview",
-	-- 	opts = {
-	-- 		-- signs_on_startup = { "all" }, -- defaults to search & diagnostics
-	-- 	},
-	-- },
+	{ -- no out-of-the-box gitsigns integration, but see my custom.plugins
+		"dstein64/nvim-scrollview",
+		opts = {
+			-- signs_on_startup = { "all" }, -- defaults to search & diagnostics
+		},
+	},
 	{ "tpope/vim-fugitive" },
 	{ "tpope/vim-rhubarb" },
 	{
