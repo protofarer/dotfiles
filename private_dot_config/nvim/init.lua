49b58cefc71417c3a3e473572c387d99d342ace7
Kenny Baron <kennybaron@fastmail.com>
2024-06-21 02:26:45 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -635,7 +635,7 @@ require("lazy").setup({
 				javascript = { { "prettierd", "prettier" } },
 				typescript = { { "prettierd", "prettier" } },
 				typescriptreact = { { "prettier" } },
-				javascriptriptreact = { { "prettierd", "prettier" } },
+				javascriptriptreact = { { "prettier" } },
 			},
 		},
 	},
