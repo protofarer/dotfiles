caa0993cd5073023cf6af75ca0f2caa37b94d6b1
Kenny Baron <kennybaron@fastmail.com>
2024-06-24 05:55:42 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -263,7 +263,7 @@ require("lazy").setup({
 	{
 		"numToStr/Comment.nvim",
 		opts = {
-			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			-- pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 		},
 	},
 
