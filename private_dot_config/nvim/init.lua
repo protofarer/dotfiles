b1654ca4a10d68c9ef099c130d62a230ea8108eb
Kenny Baron <kennybaron@fastmail.com>
2024-06-26 08:35:45 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -557,7 +557,65 @@ require("lazy").setup({
 			-- `:help lspconfig-all` for a list of all the pre-configured LSPs ,lsps
 			local servers = {
 				clangd = {},
-				tsserver = {},
+				tsserver = { enabled = false },
+				vtsls = {
+					filetypes = {
+						"javascript",
+						"javascriptreact",
+						"javascript.jsx",
+						"typescript",
+						"typescriptreact",
+						"typescript.tsx",
+					},
+					settings = {
+						complete_function_calls = true,
+						vtsls = {
+							enableMoveToFileCodeAction = true,
+							autoUseWorkspaceTsdk = true,
+							experimental = {
+								completion = {
+									enableServerSideFuzzyMatch = true,
+								},
+							},
+						},
+						typescript = {
+							updateImportsOnFileMove = { enabled = "always" },
+							suggest = {
+								completeFunctionCalls = true,
+							},
+							inlayHints = {
+								enumMemberValues = { enabled = true },
+								functionLikeReturnTypes = { enabled = true },
+								parameterNames = { enabled = "literals" },
+								parameterTypes = { enabled = true },
+								propertyDeclarationTypes = { enabled = true },
+								variableTypes = { enabled = false },
+							},
+						},
+					},
+					keys = {
+						{
+							"<leader>co",
+							LazyVim.lsp.action["source.organizeImports"],
+							desc = "Organize Imports",
+						},
+						{
+							"<leader>cM",
+							LazyVim.lsp.action["source.addMissingImports.ts"],
+							desc = "Add missing imports",
+						},
+						{
+							"<leader>cu",
+							LazyVim.lsp.action["source.removeUnused.ts"],
+							desc = "Remove unused imports",
+						},
+						{
+							"<leader>cD",
+							LazyVim.lsp.action["source.fixAll.ts"],
+							desc = "Fix all diagnostics",
+						},
+					},
+				},
 				eslint = {},
 				rust_analyzer = {},
 				ols = {},
@@ -659,6 +717,68 @@ require("lazy").setup({
 					-- Disabled to prevent "No ESLint configuration found" exceptions
 					-- 'package.json',
 				),
+			})
+
+			lspc.setup({
+				tsserver = function()
+					-- disable tsserver
+					return true
+				end,
+				vtsls = function(_, opts)
+					LazyVim.lsp.on_attach(function(client, buffer)
+						client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
+							---@type string, string, lsp.Range
+							local action, uri, range = unpack(command.arguments)
+
+							local function move(newf)
+								client.request("workspace/executeCommand", {
+									command = command.command,
+									arguments = { action, uri, range, newf },
+								})
+							end
+
+							local fname = vim.uri_to_fname(uri)
+							client.request("workspace/executeCommand", {
+								command = "typescript.tsserverRequest",
+								arguments = {
+									"getMoveToRefactoringFileSuggestions",
+									{
+										file = fname,
+										startLine = range.start.line + 1,
+										startOffset = range.start.character + 1,
+										endLine = range["end"].line + 1,
+										endOffset = range["end"].character + 1,
+									},
+								},
+							}, function(_, result)
+								---@type string[]
+								local files = result.body.files
+								table.insert(files, 1, "Enter new path...")
+								vim.ui.select(files, {
+									prompt = "Select move destination:",
+									format_item = function(f)
+										return vim.fn.fnamemodify(f, ":~:.")
+									end,
+								}, function(f)
+									if f and f:find("^Enter new path") then
+										vim.ui.input({
+											prompt = "Enter move destination:",
+											default = vim.fn.fnamemodify(fname, ":h") .. "/",
+											completion = "file",
+										}, function(newf)
+											return newf and move(newf)
+										end)
+									elseif f then
+										move(f)
+									end
+								end)
+							end)
+						end
+					end, "vtsls")
+					-- copy typescript settings to javascript
+					opts.settings.javascript =
+						vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
+				end,
 			})
 		end,
 	},
