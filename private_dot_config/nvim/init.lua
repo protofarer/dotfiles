0f4f99e2d7b1bc0ae846fde263f31bcb68669d02
Kenny Baron <kennybaron@fastmail.com>
2024-08-12 22:16:09 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1507,7 +1507,12 @@ require("lazy").setup({
 -- })
 
 -- ,colorscheme toggle
-local COLORSCHEMES = { "kanagawa", "true-monochrome", "kundalini" }
+local COLORSCHEMES = {
+	-- "kanagawa",
+	"true-monochrome",
+	"tmc",
+	-- "kundalini"
+}
 local CURR_COLORSCHEME_INDEX = 1
 
 local function get_saved_scheme()
