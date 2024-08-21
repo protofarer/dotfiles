987be7f072e8f3bc72e11d1c5e8ea9c83626f02f
Kenny Baron <kennybaron@fastmail.com>
2024-08-21 12:01:06 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1317,6 +1317,13 @@ require("lazy").setup({
 			-- use_default_keymaps = true,
 		},
 	},
+    { "sindrets/diffview.nvim", version= "*", opts = {
+        view = {
+            merge_tool = {
+                layout = "diff3_mixed"
+            }
+        }
+    }}
 	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
