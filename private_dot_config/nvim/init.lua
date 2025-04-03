ad0a96f2f7abee706649b5a0a38a259771d24014
Kenny Baron <kennybaron@fastmail.com>
2025-04-03 03:37:35 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1537,27 +1537,23 @@ vim.keymap.set("n", "<leader>tc", ":ToggleColorscheme<CR>", { desc = "Toggle col
 
 vim.cmd("colorscheme " .. COLORSCHEMES[get_saved_scheme()])
 
-vim.api.nvim_create_user_command("CheckHighlight", function()
-	local word = vim.fn.expand("<cword>")
+vim.api.nvim_create_user_command("SyntaxInfo", function()
 	local line = vim.fn.line(".")
 	local col = vim.fn.col(".")
-	local ns_id = vim.api.nvim_get_namespaces()["treesitter"]
 
 	-- Get syntax ID at cursor
 	local syntax_id = vim.fn.synID(line, col, 1)
 	local syntax_name = vim.fn.synIDattr(syntax_id, "name")
+	local trans_id = vim.fn.synIDtrans(syntax_id)
+	local trans_name = vim.fn.synIDattr(trans_id, "name")
 
-	-- Get treesitter highlight group at cursor
-	local ts_captures = vim.treesitter.get_captures_at_pos(0, line - 1, col - 1)
+	-- Get colors
+	local fg = vim.fn.synIDattr(trans_id, "fg")
+	local bg = vim.fn.synIDattr(trans_id, "bg")
 
-	print("Syntax ID: " .. syntax_name)
-	print("Treesitter captures:")
-	vim.pretty_print(ts_captures)
-
-	-- Get highlight information
-	local hl = vim.api.nvim_get_hl(0, { name = syntax_name })
-	print("Highlight attributes:")
-	vim.pretty_print(hl)
+	print("Syntax name: " .. syntax_name)
+	print("Translated name: " .. trans_name)
+	print("Foreground color: " .. (fg or "none"))
+	print("Background color: " .. (bg or "none"))
 end, {})
-
 -- vim: ts=4 sts=4 sw=4 et
