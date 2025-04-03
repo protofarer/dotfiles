4a8cb651c63fee109800696ed6d946ccb4ded631
Kenny Baron <kennybaron@fastmail.com>
2025-04-03 04:06:36 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1522,6 +1522,24 @@ local function save_scheme(index)
 	end
 end
 
+local function fix_kundalini_comments()
+	-- Red color for comments (using your color values)
+	local comment_fg = "#8B0000" -- Adjust if needed to match your red
+	local comment_bg = "#0B0000" -- Adjust if needed to match your black
+
+	-- Set all comment highlight groups
+	vim.api.nvim_set_hl(0, "Comment", { fg = comment_fg, bg = comment_bg, italic = true })
+	vim.api.nvim_set_hl(0, "@comment", { fg = comment_fg, bg = comment_bg, italic = true })
+	vim.api.nvim_set_hl(0, "TSComment", { fg = comment_fg, bg = comment_bg, italic = true })
+
+	-- Additional comment groups that might be used
+	vim.api.nvim_set_hl(0, "@comment.documentation", { fg = comment_fg, bg = comment_bg, italic = true })
+	vim.api.nvim_set_hl(0, "@comment.error", { fg = comment_fg, bg = comment_bg, italic = true })
+	vim.api.nvim_set_hl(0, "@comment.warning", { fg = comment_fg, bg = comment_bg, italic = true })
+	vim.api.nvim_set_hl(0, "@comment.todo", { fg = comment_fg, bg = comment_bg, italic = true })
+	vim.api.nvim_set_hl(0, "@comment.note", { fg = comment_fg, bg = comment_bg, italic = true })
+end
+
 local function toggle_colorscheme()
 	CURR_COLORSCHEME_INDEX = get_saved_scheme()
 	CURR_COLORSCHEME_INDEX = (CURR_COLORSCHEME_INDEX % #COLORSCHEMES) + 1
@@ -1554,23 +1572,5 @@ vim.api.nvim_create_user_command("DiagnoseComments", function()
 		vim.print(hl)
 	end
 end, {})
-
-local function fix_kundalini_comments()
-	-- Red color for comments (using your color values)
-	local comment_fg = "#8B0000" -- Adjust if needed to match your red
-	local comment_bg = "#0B0000" -- Adjust if needed to match your black
-
-	-- Set all comment highlight groups
-	vim.api.nvim_set_hl(0, "Comment", { fg = comment_fg, bg = comment_bg, italic = true })
-	vim.api.nvim_set_hl(0, "@comment", { fg = comment_fg, bg = comment_bg, italic = true })
-	vim.api.nvim_set_hl(0, "TSComment", { fg = comment_fg, bg = comment_bg, italic = true })
-
-	-- Additional comment groups that might be used
-	vim.api.nvim_set_hl(0, "@comment.documentation", { fg = comment_fg, bg = comment_bg, italic = true })
-	vim.api.nvim_set_hl(0, "@comment.error", { fg = comment_fg, bg = comment_bg, italic = true })
-	vim.api.nvim_set_hl(0, "@comment.warning", { fg = comment_fg, bg = comment_bg, italic = true })
-	vim.api.nvim_set_hl(0, "@comment.todo", { fg = comment_fg, bg = comment_bg, italic = true })
-	vim.api.nvim_set_hl(0, "@comment.note", { fg = comment_fg, bg = comment_bg, italic = true })
-end
 
 -- vim: ts=4 sts=4 sw=4 et
