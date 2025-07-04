922c304a77513af60e26c72cd3915d760e22592c
Kenny Baron <kennybaron@fastmail.com>
2025-07-04 11:29:16 -0400

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1620,6 +1620,67 @@ require("lazy").setup({
 				{ "<leader>ps", ":MarkdownPreviewStop<CR>", { desc = "Stop markdown preview browser" } },
 			},
 		},
+		{
+			"ThePrimeagen/harpoon",
+			branch = "harpoon2",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			config = function()
+				local harpoon = require("harpoon")
+				harpoon:setup()
+				-- basic telescope configuration
+				local conf = require("telescope.config").values
+				local function toggle_telescope(harpoon_files)
+					local file_paths = {}
+					for _, item in ipairs(harpoon_files.items) do
+						table.insert(file_paths, item.value)
+					end
+
+					require("telescope.pickers")
+						.new({}, {
+							prompt_title = "Harpoon",
+							finder = require("telescope.finders").new_table({
+								results = file_paths,
+							}),
+							previewer = conf.file_previewer({}),
+							sorter = conf.generic_sorter({}),
+						})
+						:find()
+				end
+
+				-- instead of harpoon ui quick menu
+				vim.keymap.set("n", "<C-y>", function()
+					toggle_telescope(harpoon:list())
+				end, { desc = "Open harpoon window" })
+				-- replaced by telescope above menu
+				-- vim.keymap.set("n", "<C-y>", function()
+				-- 	harpoon.ui:toggle_quick_menu(harpoon:list())
+				-- end)
+
+				vim.keymap.set("n", "<leader>a", function()
+					harpoon:list():add()
+				end)
+				vim.keymap.set("n", "<C-r>", function()
+					harpoon:list():select(1)
+				end)
+				vim.keymap.set("n", "<C-t>", function()
+					harpoon:list():select(2)
+				end)
+				vim.keymap.set("n", "<C-u>", function()
+					harpoon:list():select(3)
+				end)
+				vim.keymap.set("n", "<C-i>", function()
+					harpoon:list():select(4)
+				end)
+
+				-- Toggle previous & next buffers stored within Harpoon list
+				vim.keymap.set("n", "<C-S-P>", function()
+					harpoon:list():prev()
+				end)
+				vim.keymap.set("n", "<C-S-N>", function()
+					harpoon:list():next()
+				end)
+			end,
+		},
 		-- {
 		-- 	"greggh/claude-code.nvim",
 		-- 	dependencies = {
