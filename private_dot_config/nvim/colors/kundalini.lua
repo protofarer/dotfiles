2c6917f945bfe68780881a566b753e51fb6deeca
Kenny Baron <kennybaron@fastmail.com>
2026-01-04 22:53:21 -0500

    Update .config/nvim/colors/kundalini.lua

--- private_dot_config/nvim/colors/kundalini.lua
+++ private_dot_config/nvim/colors/kundalini.lua
@@ -35,7 +35,7 @@ Color.new("green", "#00FF00")
 
 Group.new("Comment", c.grey, c.black, s.italic)
 Group.new("@comment", c.grey, c.black, s.italic)
-Group.new("TodoFg", c.green, nil, s.italic) -- TODO comment plugin
+Group.new("TodoFgTODO", c.green, nil, s.italic) -- TODO comment plugin
 -- Group.new("TodoBg", c.grey, nil, s.italic) -- TODO comment plugin
 
 -- std style, normal code
