513adc9748de783529c0905fdaed125d48b33cb2
Kenny Baron <kennybaron@fastmail.com>
2026-01-04 23:02:54 -0500

    Update .config/nvim/colors/kundalini.lua

--- private_dot_config/nvim/colors/kundalini.lua
+++ private_dot_config/nvim/colors/kundalini.lua
@@ -36,7 +36,7 @@ Color.new("green", "#00FF00")
 Group.new("Comment", c.grey, c.black, s.italic)
 Group.new("@comment", c.grey, c.black, s.italic)
 Group.new("TodoFgTODO", c.light, nil, s.italic) -- TODO comment plugin; the colon and comment itself
-Group.new("TodoBgTODO", c.light, nil, s.underline) -- TODO comment plugin; the text TODO itself
+Group.new("TodoBgTODO", c.grey, nil, s.underline) -- TODO comment plugin; the text TODO itself
 
 -- std style, normal code
 Group.new("@variable", c.grey, c.black, s.NONE)
