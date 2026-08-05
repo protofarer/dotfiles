26fab3708eb6ca4e8987d04f3ef321819b23e459
Kenny Baron <kennybaron@fastmail.com>
2026-08-05 10:11:17 -0400

    Update .config/nvim/colors/jb.lua

--- private_dot_config/nvim/colors/jb.lua
+++ private_dot_config/nvim/colors/jb.lua
@@ -52,7 +52,7 @@ Color.new("cyan", "#88dcbd")
 Color.new("teal", "#39cc98") -- strings, prolly dont dim so much
 
 -- return keyword
-Color.new("red", "#b53d3d")
+Color.new("red", "#e53d3d")
 
 -- Editor
 Color.new("bright_amber", "#e28513")
